527ac2cd36809c9b99b6049134bad3eed49689fd
Ed <edward.guiness@gmail.com>
2013-07-08 17:42:19 +0100

    Updated to v15, included █████ symbol

--- code/popup.js
+++ code/popup.js
@@ -1,15 +1,4 @@
-
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36852229-2']);
-_gaq.push(['_trackPageview']);
-
 $(function() {
-
-    $("body").fadeOut(10, function() { $(this).show(); }); 
-
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 
     $("#tabs").tabs();
 
@@ -21,9 +10,6 @@ $(function() {
         function() {$(this).css( {'cursor':'auto', 'background-color':''});}
     )
     .click(function() {
-
-        _gaq.push(['_trackEvent', $(this).text(), 'clicked']);
-
         var bgPage = chrome.extension.getBackgroundPage();
         bgPage.copy($(this).text());
 
